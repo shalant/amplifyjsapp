2fcff0a0965c10700fe12f3af0289be577e8d223
shalant <doug.rosenberg@gmail.com>
2022-06-02 22:41:15 -0500

    finished a very basic tutorial

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,8 @@
+export type AmplifyDependentResourcesAttributes = {
+    "api": {
+        "amplifyjsapp": {
+            "GraphQLAPIIdOutput": "string",
+            "GraphQLAPIEndpointOutput": "string"
+        }
+    }
+}
